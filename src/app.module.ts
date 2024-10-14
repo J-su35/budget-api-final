61fe757b118b4fbf22fcfe733225911e535db0bd
J-su35 <det_a248@hotmail.com>
2024-10-14 22:55:27 +0700

    create users module and bcrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ItemsModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    DbModule
+    DbModule,
+    UsersModule
   ]
 })
 export class AppModule {}
