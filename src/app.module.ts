bff0e0ae16baae10ede77e28d4588db41039ad03
J-su35 <det_a248@hotmail.com>
2024-10-14 18:26:22 +0700

    gen db module+typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [ItemsModule,
-    ConfigModule.forRoot({ isGlobal: true })
+    ConfigModule.forRoot({ isGlobal: true }),
+    DbModule
   ]
 })
 export class AppModule {}
