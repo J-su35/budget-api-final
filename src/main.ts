6e6078210af04968b78cb9cb17f7b4527bff7476
J-su35 <det_a248@hotmail.com>
2024-10-18 22:19:40 +0700

    add query-failed-error.filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { QueryFailedErrorFilter } from './filters/query-failed-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new QueryFailedErrorFilter(httpAdapter))
   await app.listen(3000);
 }
 bootstrap();
